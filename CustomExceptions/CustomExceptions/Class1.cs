003a53d8c4fdd373b34817a4a80c9239939453cb
shibathethinker <shibassmb@gmail.com>
2017-02-26 12:07:58 +1100

    Exception Classes

---
+++ CustomExceptions/CustomExceptions/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CustomExceptions
+{
+    /*
+     * All the custom exceptions to be used/created in this project
+     * should be defined here
+     * 
+     * @Author: shibasis sengupta
+    */
+
+    public class invalidParamException:System.Exception
+    {
+        private String desc="Incomplete or Invalid Parameter Passed";
+       
+        public invalidParamException(String src)
+        {
+            desc = desc + " source: " + src;
+        }
+        
+        public String toString()
+        {
+            return desc;
+        }
+
+
+     }
+
+    public class businessRuleViolationException : System.Exception
+    {
+        private String desc = "In conflict with business rules";
+
+        public businessRuleViolationException(String src)
+        {
+            desc = desc + " source: " + src;
+        }
+        public String toString()
+        {
+            return desc;
+        }
+    }
+}
